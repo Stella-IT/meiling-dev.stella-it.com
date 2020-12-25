e2aca2c6fc36ecaaaa4218b784f74e9b37a025c9
O-Kreator <dongjunkang.design@gmail.com>
2020-12-25 23:34:45 +0900

    fix: add alt attribute to ProfileImg atom component

--- src/components/atoms/ProfileImg/index.tsx
+++ src/components/atoms/ProfileImg/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './ProfileImg.scss';
 
 interface Props {
+  alt?: string;
   src?: string;
   size?: "small" | "large";
 }
 
 const ProfileImg: React.FC<Props> = ({
+  alt = "",
   src = "",
   size = "large",
   ...props
 }) => {
-  return (<img className={`profile_img ${size}`} src={src} {...props} />)
+  return (<img className={`profile_img ${size}`} src={src} alt={alt} {...props} />)
 }
 
 export default ProfileImg;
