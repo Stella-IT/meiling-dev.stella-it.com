4a6e013d1ac34b05f860428387ca7a2bb84bd693
O-Kreator <dongjunkang.design@gmail.com>
2021-01-08 06:01:20 +0900

    fix: use SocialBtnsGroup component in SocialSignIn

--- src/components/pages/SocialSignIn/index.tsx
+++ src/components/pages/SocialSignIn/index.tsx
@@ -2,20 +2,14 @@ import React, { Fragment } from 'react';
 
 import ContentWrapper from '../../templates/ContentWrapper';
 import Btn from '../../atoms/Btn';
+import SocialBtnsGroup from '../../molecules/SocialBtnsGroup';
 
 const SocialSignIn: React.FC = () => {
   return (
     <Fragment>
       <ContentWrapper
         progressDisabled
-        content={
-          <>
-            <Btn>1</Btn>
-            <Btn>2</Btn>
-            <Btn>3</Btn>
-            <Btn>4</Btn>
-          </>
-        }
+        content={<SocialBtnsGroup />}
         buttonsBottom={[
           <Btn styleType="secondary" to="/">아이디나 이메일로 로그인</Btn>
         ]}
