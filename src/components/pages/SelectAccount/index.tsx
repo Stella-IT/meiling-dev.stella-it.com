bea2000d010fd1e528748b363a1f4e200596067b
Alex4386 <psh010209@gmail.com>
2021-01-14 05:06:33 +0900

    fix: loadState was not properly updated

--- src/components/pages/SelectAccount/index.tsx
+++ src/components/pages/SelectAccount/index.tsx
@@ -29,7 +29,7 @@ const SelectAccount: React.FC<Props> = ({
           });
         } catch(e) {
           setLoadState({
-            loaded: false,
+            loaded: true,
             error: true,
             users: [],
           });
