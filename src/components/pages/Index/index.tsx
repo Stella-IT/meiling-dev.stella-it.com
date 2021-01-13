233ff1c1f96277f6fbfb40accc96bf0d539f7e57
Alex4386 <psh010209@gmail.com>
2021-01-14 02:30:09 +0900

    feat: add new index page

---
+++ src/components/pages/Index/index.tsx
@@ -0,0 +1,48 @@
+import React, { Fragment } from 'react';
+
+import { getMeilingSessionToken } from '../../../common/';
+import ContentWrapper from '../../templates/ContentWrapper';
+import Btn from '../../atoms/Btn';
+import './Index.scss';
+import { RouteComponentProps } from 'react-router-dom';
+
+interface Props extends RouteComponentProps {
+  
+};
+
+const Index: React.FC<Props> = () => {
+  return (
+    <Fragment>
+      <ContentWrapper
+        pageName="index"
+        progressValue={1 / 10 * 100}
+        content={
+          <>
+            <h1>테스트 모드</h1>
+            <p>테스트 모드에 접속하셨습니다.</p>
+
+            <h2>접속 가능한 페이지</h2>
+            <div className='btn_column_wrapper'>
+              <Btn to="signin" grow styleType="secondary">로그인</Btn>
+              <Btn to="socialsignin" grow styleType="secondary">소셜 계정으로 로그인</Btn>
+              <Btn to="password" grow styleType="secondary">패스워드 입력</Btn>
+              <Btn onClick={async () => {
+                console.log("working on it!");
+                const token = await getMeilingSessionToken();
+
+                alert(token);
+              }} grow styleType="secondary">메이링 서버 세션토큰 발급 테스트</Btn>
+              <Btn href="https://github.com/Stella-IT/meiling-dev.stella-it.com" grow styleType="secondary">깃허브 레포지토리 접속</Btn>
+            </div>
+          </>
+        }
+        buttonsBottom={[
+          <Btn key="button_developers" styleType="secondary" href="https://opensource.stella-it.com/developers/">개발자 리소스</Btn>,
+          <Btn key="button_signin" to="signin">로그인</Btn>
+        ]}
+      />
+    </Fragment>
+  );
+}
+
+export default Index;
