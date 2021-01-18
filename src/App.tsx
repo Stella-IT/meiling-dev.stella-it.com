b66958b8bf58a149d8f791867b440f315064b854
Alex4386 <psh010209@gmail.com>
2021-01-18 10:53:02 +0900

    feat: join the dark side.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, Context, createContext } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
@@ -16,59 +16,57 @@ import TestMode from './components/pages/TestMode';
 import Users from './components/pages/Users';
 import TwoFactor from './components/pages/TwoFactor';
 
-// TODO: Fix later
-interface AppSessionData {
-  login?: {
-    username: string;
-    twoFactorType?: string;
-  }
-}
-
-interface AppSessionObject {
-  session?: AppSessionData;
-  setSession?: (session: AppSessionData) => void;
-}
-
-export const AppSession: Context<AppSessionObject> = createContext({});
-
 function App() {
-  const [session, setSession] = useState({});
+  
+  const darkThemeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const isDarkMode = () => darkThemeMediaQuery.matches;
+  
+  const [theme, setTheme] = useState(isDarkMode() ? "dark" : "light");
+
+  darkThemeMediaQuery.addEventListener("change", () => {
+    setTheme(isDarkMode() ? "dark" : "light");
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.remove("theme-light");
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+      document.body.classList.add("theme-light");
+    }
+  });
 
   return (
-    <AppSession.Provider value={{
-      session,
-      setSession
-    }}>
-      <Fragment>
-        <LogoImg />
-        <Router>
-          
-          <Route render={({location}) => {
-            console.log(location.key);
-            return (
-              <TransitionGroup>
-                <CSSTransition key={location.key} timeout={400} classNames="page">
+    <Fragment>
+      <LogoImg />
+      <Router>
+        
+        <Route render={({location}) => {
+          console.log(location.key);
+          return (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={400} classNames="page">
 
-                  <Switch location={location}>
-                    <Route exact path="/" component={Index} />
-                    <Route exact path="/signin" component={SignIn} />
-                    <Route exact path="/signout" component={SignOut} />
-                    <Route exact path="/socialsignin" component={SocialSignIn} />
-                    <Route exact path="/password" component={Password} />
-                    <Route exact path="/testmode" component={TestMode} />
-                    <Route exact path="/users" component={Users} />
-                    <Route exact path="/2fa" component={TwoFactor} />
-                    <Route exact path="/2fa/:method" component={TwoFactor} />
-                  </Switch>
+                <Switch location={location}>
+                  <Route exact path="/" component={Index} />
+                  <Route exact path="/signin" component={SignIn} />
+                  <Route exact path="/signout" component={SignOut} />
+                  <Route exact path="/socialsignin" component={SocialSignIn} />
+                  <Route exact path="/password" component={Password} />
+                  <Route exact path="/testmode" component={TestMode} />
+                  <Route exact path="/users" component={Users} />
+                  <Route exact path="/2fa" component={TwoFactor} />
+                  <Route exact path="/2fa/:method" component={TwoFactor} />
+                </Switch>
 
-                </CSSTransition>
-              </TransitionGroup>
-            )
-          }} />
-          
-        </Router>
-      </Fragment>
-    </AppSession.Provider>
+              </CSSTransition>
+            </TransitionGroup>
+          )
+        }} />
+        
+      </Router>
+    </Fragment>
   );
 }
 
